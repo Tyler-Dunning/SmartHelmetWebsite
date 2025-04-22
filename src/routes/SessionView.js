868576a36210383609a269e29ba45c45640a39f1
Tyler-Dunning <118504218+Tyler-Dunning@users.noreply.github.com>
2025-04-22 12:17:11 -0700

    Session changes first draft

--- src/routes/SessionView.js
+++ src/routes/SessionView.js
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import { firestore } from './firebase';
-import React from 'react';
+import React, { useState } from 'react';
 import { collection } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
 
@@ -10,6 +10,8 @@ function HelmetView() {
   const navigate = useNavigate();
   const { state } = useLocation();
   const { helmetNo, sessionNo } = state;
+
+  const [expandedId, setExpandedId] = useState(null); // track expanded item
 
   const [value, loading, error] = useCollection(
     collection(firestore, 'helmets', helmetNo, sessionNo),
@@ -23,14 +25,45 @@ function HelmetView() {
 
   return (
     <div className="session-detail-wrapper">
-      <button className="session-back-button" onClick={() => navigate('/helmet', { state: { helmetNo } })}>Back to Helmet {helmetNo}</button>
+      <button
+        className="session-back-button"
+        onClick={() => navigate('/helmet', { state: { helmetNo } })}
+      >
+        Back to Helmet {helmetNo}
+      </button>
       <h2 className="session-title">{sessionNo} Data</h2>
       <ul className="session-data-list">
-        {value.docs.map((doc) => (
-          <li className="session-data-item" key={doc.id}>
-            <pre>{JSON.stringify(doc.data(), null, 2)}</pre>
-          </li>
-        ))}
+        {value.docs.map((doc) => {
+          const data = doc.data();
+          const { accMag, gyroMag, severity, content } = data['res'];
+
+          const isExpanded = expandedId === doc.id;
+
+          return (
+            <li
+              key={doc.id}
+              className={`session-data-item ${
+                severity === 'high'
+                  ? 'severity-high'
+                  : severity === 'medium'
+                  ? 'severity-medium'
+                  : 'severity-low'
+              } ${isExpanded ? 'expanded' : ''}`}
+              onClick={() => setExpandedId(isExpanded ? null : doc.id)}
+            >
+              <div><h2>{severity} Severity</h2></div>
+              <div><strong>Acceleration Magnitude:</strong> {Math.round(accMag)}</div>
+              <div><strong>Gyroscope Magnitude:</strong> {Math.round(gyroMag)}</div>
+              {!isExpanded && <p>Click to see more</p>}
+              {isExpanded && (
+                <div className="raw-data"><strong>Raw Content:</strong> <pre>{JSON.stringify(content, null, 2)}</pre></div>
+              )}
+              {isExpanded && (
+                <p>Click to see less</p>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
